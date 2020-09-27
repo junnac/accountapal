55170f100373749c27b7a56ccf097ee10968c75c
Joanna Chen <x.chenj@gmail.com>
2020-09-27 13:33:01 -0700

    Move database connection to login page

--- pages/index.js
+++ pages/index.js
@@ -1,44 +1,16 @@
 import Head from 'next/head';
-import { connectToDatabase } from '../util/mongodb'
-import { signIn, signOut, useSession } from 'next-auth/client';
+import Link from 'next/link';
 
-const Home = ({ isConnected }) => {
-  const [session, loading] = useSession();
-
-  if (!isConnected) {
-    return (
-      <h1>MongoDB not connected!</h1>
-    );
-  }
-  
+const Home = () => {
   return (
     <>
       <Head>
         <title>AccountaPal</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      {!session && (
-        <>
-          Not signed in <br />
-          <button onClick={signIn}>Sign in</button>
-        </>
-      )}
-      {session && (
-        <>
-          Signed in as {session.user.email} <br />
-          <button onClick={signOut}>Sign out</button>
-        </>
-      )}
+      <Link href="/login">Login</Link>
     </>
   );
-};
-
-export const getServerSideProps = async (context) => {
-  const { client } = await connectToDatabase();
-  const isConnected = await client.isConnected(); // Returns true or false
-  return {
-    props: { isConnected },
-  };
 };
 
 export default Home;
---
+++ pages/login.js
@@ -0,0 +1,42 @@
+import Head from 'next/head';
+import { connectToDatabase } from '../util/mongodb'
+import { signIn, signOut, useSession } from 'next-auth/client';
+
+const Login = ({ isConnected  }) => {
+  const [session, loading] = useSession();
+
+  if (!isConnected) {
+    return <h1>MongoDB not connected!</h1>;
+  }
+  
+  return (
+    <>
+      <Head>
+        <title>AccountaPal</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      {!session && (
+        <>
+          Not signed in <br />
+          <button onClick={signIn}>Sign in</button>
+        </>
+      )}
+      {session && (
+        <>
+          Signed in as {session.user.email} <br />
+          <button onClick={signOut}>Sign out</button>
+        </>
+      )}
+    </>
+  );
+};
+
+export const getServerSideProps = async (context) => {
+  const { client } = await connectToDatabase();
+  const isConnected = await client.isConnected(); // Returns true or false
+  return {
+    props: { isConnected },
+  };
+};
+
+export default Login;
